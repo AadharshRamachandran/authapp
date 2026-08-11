e247f97cb8f0cacc8f87df400007d1c796bd7c1e
Aadharsh Ramachandran <aadharshramachandran04@gmail.com>
2026-08-11 14:25:30 +0530

    second commit

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const { getAuth } = require('firebase-admin/auth');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 const app = admin.initializeApp({
   credential: admin.cert(serviceAccount)
